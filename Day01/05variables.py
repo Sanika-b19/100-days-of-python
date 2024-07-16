9e5bcd38d227e7806e379d9e50cc5bf050739b63
Sanika-b19 <117573899+Coddersoul19@users.noreply.github.com>
2024-07-16 18:24:39 +0530

    Create 05variables.py

---
+++ Day01/05variables.py
@@ -0,0 +1,12 @@
+
+# Defining Variables
+name ="Alice"
+age = 21
+height = 5.5
+is_student = True
+
+# Printing Variables
+print("Name = ",name)
+print("Age = ",age)
+print("Height = ",height)
+print("Is a student = ",is_student)
