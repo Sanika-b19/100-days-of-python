db6a4a7cc1af68351612bf7792d49ff04a989745
Sanika-b19 <117573899+Coddersoul19@users.noreply.github.com>
2024-07-16 18:24:25 +0530

    Create 04print_statement.py

---
+++ Day01/04print_statement.py
@@ -0,0 +1,6 @@
+
+print("hello, world")               # Basic print Statement
+print("hello","world",sep=",")      # Using Seperator
+print("hello"," world",end="!!\n")  # Using End parameter
+name = "Alice"
+print(f"Hello {name}!")              #Using Formatted String
