05e9dde7fd88da766dbcfb7159b7f7bbc443dd06
Sanika-b19 <117573899+Coddersoul19@users.noreply.github.com>
2024-07-16 18:24:09 +0530

    Create 03escape_sequence.py

---
+++ Day01/03escape_sequence.py
@@ -0,0 +1,6 @@
+
+print("hello, World")         #Normal String
+print("hello","\nworld")      #New Line
+print("hello","\tWorld")      #Tab Character
+print("\"hello world\"")      #string inside double quote
+print("\'hello world\'")      #string inside single quote
