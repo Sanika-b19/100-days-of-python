328e5c011a03b6e22ba7f4795344d6f85f59e923
Sanika-b19 <117573899+Coddersoul19@users.noreply.github.com>
2024-07-17 11:52:36 +0530

    Create 02operators.py

---
+++ Day02/02operators.py
@@ -0,0 +1,24 @@
+# Arithmetic operators
+a = 10
+b = 5
+
+print("Addition:", a + b)        # 15
+print("Subtraction:", a - b)     # 5
+print("Multiplication:", a * b)  # 50
+print("Division:", a / b)        # 2.0
+print("Floor Division:", a // b) # 2
+print("Modulus:", a % b)         # 0
+print("Exponent:", a ** b)       # 100000
+
+# Comparison operators
+print("Equal:", a == b)          # False
+print("Not equal:", a != b)      # True
+print("Greater than:", a > b)    # True
+print("Less than:", a < b)       # False
+print("Greater than or equal to:", a >= b) # True
+print("Less than or equal to:", a <= b)    # False
+
+# Logical operators
+print("Logical AND:", a > 0 and b > 0) # True
+print("Logical OR:", a > 0 or b < 0)  # True
+print("Logical NOT:", not(a > 0))     # False
