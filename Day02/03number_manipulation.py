7ec00528bf5b4e8308a58b248db381ad78b29247
Sanika-b19 <117573899+Coddersoul19@users.noreply.github.com>
2024-07-17 11:52:58 +0530

    Create 03number_manipulation.py

---
+++ Day02/03number_manipulation.py
@@ -0,0 +1,26 @@
+# Basic number manipulation in Python
+num = 10
+
+# Increment
+num += 1  # num is now 11
+print("Incremented number:", num)
+
+# Decrement
+num -= 2  # num is now 9
+print("Decremented number:", num)
+
+# Multiplication
+num *= 3  # num is now 27
+print("Multiplied number:", num)
+
+# Division
+num /= 3  # num is now 9.0
+print("Divided number:", num)
+
+# Power
+num = num ** 2  # num is now 81.0
+print("Number raised to power:", num)
+
+# Rounding numbers
+print("Rounded number:", round(num))       # 81
+print("Rounded to 2 decimal places:", round(num, 2))  # 81.0
