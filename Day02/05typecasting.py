be4e303752b1036dec9d220819aa8aacb8330fb8
Sanika-b19 <117573899+Coddersoul19@users.noreply.github.com>
2024-07-17 11:54:44 +0530

    Create 05typecasting.py

---
+++ Day02/05typecasting.py
@@ -0,0 +1,27 @@
+# Implicit Typecasting
+num_int = 123
+num_float = 1.23
+num_new = num_int + num_float
+
+print("Value of num_new:", num_new)
+print("Data type of num_new:", type(num_new))
+
+# Explicit Typecasting
+# Converting float to int
+num_float = 3.56
+num_int = int(num_float)
+print("Value of num_int:", num_int)
+print("Data type of num_int:", type(num_int))
+
+# Converting string to int
+num_str = "456"
+num_int = int(num_str)
+print("Value of num_int:", num_int)
+print("Data type of num_int:", type(num_int))
+
+# Converting int to string
+num = 789
+num_str = str(num)
+print("Value of num_str:", num_str)
+print("Data type of num_str:", type(num_str))
+
