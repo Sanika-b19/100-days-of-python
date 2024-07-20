a615486c6f01f66935e5f1b2f778a9f5f289dca3
Sanika-b19 <117573899+Coddersoul19@users.noreply.github.com>
2024-07-20 18:31:47 +0530

    Create 03for_loop.py

---
+++ Day05/03for_loop.py
@@ -0,0 +1,13 @@
+# For loop in Python.
+
+example_list = [1, 2, 3, 4, 5]
+print("For Loop with Lists:")
+for item in example_list:
+    print(f"Item: {item}")
+
+# Finding the maximum item
+max_item = example_list[0]
+for item in example_list:
+    if item > max_item:
+        max_item = item
+print(f"Maximum item: {max_item}")
