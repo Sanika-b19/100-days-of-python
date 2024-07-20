b225c4127a437ba3ef1c39f50f953de196f8aa82
Sanika-b19 <117573899+Coddersoul19@users.noreply.github.com>
2024-07-20 18:28:29 +0530

    Create 02string_methods.py

---
+++ Day05/02string_methods.py
@@ -0,0 +1,10 @@
+# string methods in python
+
+
+example_string = "Hello, World!"
+print("String Methods:")
+print(example_string.lower())     # Convert to lowercase
+print(example_string.upper())     # Convert to uppercase
+print(example_string.replace("World", "Python"))  # Replace substring
+print(example_string.find("World"))  # Find the position of substring
+print(example_string.count("l"))   # Count occurrences of a character
