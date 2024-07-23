5ac4053499f854e59e75b78bd712701ac6350e49
Sanika-b19 <117573899+Coddersoul19@users.noreply.github.com>
2024-07-23 23:45:42 +0530

    Delete 03bmi_calculator.py

--- Day08/Exercise/03bmi_calculator.py
+++
@@ -1,9 +0,0 @@
-# Exercise : BMI Calculator
-
-weight = float(input("Enter your weight in kg: "))
-height = float(input("Enter your height in meters: "))
-
-# Calculate BMI
-bmi = weight / (height ** 2)
-
-print(f"Your BMI is {bmi:.2f}.")
