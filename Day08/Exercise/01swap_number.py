ea391a2e0769ece4862337dcdbf5b003d3231032
Sanika-b19 <117573899+Coddersoul19@users.noreply.github.com>
2024-07-23 22:55:09 +0530

    Create 01swap_number.py

---
+++ Day08/Exercise/01swap_number.py
@@ -0,0 +1,19 @@
+# Exercise: Swapping Two Numbers
+
+
+# Function to swap two numbers
+def swap(a, b):
+    a, b = b, a
+    return a, b
+
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# before swapping
+print(f"Before swapping: num1 = {num1}, num2 = {num2}")
+
+num1, num2 = swap(num1, num2)
+
+# after swapping
+print(f"After swapping: num1 = {num1}, num2 = {num2}")
