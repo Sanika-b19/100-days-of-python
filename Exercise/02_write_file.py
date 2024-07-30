282a29fd5ff05729373df1f14b408e78ef094877
Sanika Bagul <117573899+Sanika-b19@users.noreply.github.com>
2024-07-31 00:00:37 +0530

    Delete Exercise directory

--- Exercise/02_write_file.py
+++
@@ -1,7 +0,0 @@
-# Exercise 2: Writing to a File
-
-def write_to_file(file_path, content):
-    with open(file_path, 'w') as file:
-        file.write(content)
-
-write_to_file('output.txt', 'Hello, World!')
