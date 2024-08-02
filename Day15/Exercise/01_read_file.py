2f165b0633c752044be2a45004768294c31b2ca4
Sanika Bagul <117573899+Sanika-b19@users.noreply.github.com>
2024-08-02 22:37:03 +0530

    Delete Day15/Exercise directory

--- Day15/Exercise/01_read_file.py
+++
@@ -1,8 +0,0 @@
-# Exercise 1: Reading from a File
-
-def read_file(file_path):
-    with open(file_path, 'r') as file:
-        content = file.read()
-        print(content)
-
-read_file('example.txt')
--- Day15/Exercise/03_append_file.py
+++
@@ -1,7 +0,0 @@
-# Exercise 3: Appending to a File
-
-def append_to_file(file_path, content):
-    with open(file_path, 'a') as file:
-        file.write(content)
-
-append_to_file('output.txt', '\nHello, again!')
