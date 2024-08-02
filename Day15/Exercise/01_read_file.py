ab5ad72bf9f68537bec35b15309fa2b3749f9c02
Sanika Bagul <117573899+Sanika-b19@users.noreply.github.com>
2024-08-02 22:38:34 +0530

    Add files via upload

---
+++ Day15/Exercise/01_read_file.py
@@ -0,0 +1,8 @@
+# Exercise 1: Reading from a File
+
+def read_file(file_path):
+    with open(file_path, 'r') as file:
+        content = file.read()
+        print(content)
+
+read_file('example.txt')
